18f3d09134592ddfb601fd515f0b3725a42cc5de
SandyAbdullahi <ab2d222@gmail.com>
2025-08-03 16:44:02 +0300

    CI: call django.setup() before importing models in tests

--- tests/test_summary_endpoint.py
+++ tests/test_summary_endpoint.py
@@ -1,14 +1,14 @@
 # tests/test_summary_endpoint.py
 # import json
-import pytest
-
-pytestmark = pytest.mark.django_db
 
 from datetime import date
 
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
+import pytest
+
+pytestmark = pytest.mark.django_db
 
 from finance.models import Category, Transaction
 
