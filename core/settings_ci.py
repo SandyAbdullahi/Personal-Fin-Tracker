9890c97696a33f6ee03dc08123e466f2e9f090ec
SandyAbdullahi <ab2d222@gmail.com>
2025-08-04 09:07:37 +0300

    CI: call django.setup() before importing models in tests

--- core/settings_ci.py
+++ core/settings_ci.py
@@ -17,20 +17,20 @@ DATABASES = {
     }
 }
 #
-# INSTALLED_APPS = [
-#     "django.contrib.contenttypes",
-#     "django.contrib.admin",
-#     "django.contrib.auth",
-#     "django.contrib.sessions",
-#     "django.contrib.messages",
-#     "django.contrib.staticfiles",
-#     "rest_framework",
-#     "rest_framework_simplejwt",
-#     "django_filters",
-#     # Custom apps
-#     "accounts",
-#     "finance",
-# ]
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "django_filters",
+    # Custom apps
+    "accounts",
+    "finance",
+]
 
 
 # ------------------------------------------------------------------
