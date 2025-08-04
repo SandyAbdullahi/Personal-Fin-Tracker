fa0555eb6cefe6925a37081bf9f1ce7428d478fb
SandyAbdullahi <ab2d222@gmail.com>
2025-08-04 12:03:25 +0300

    CI: satisfy admin templates requirement

--- core/settings_ci.py
+++ core/settings_ci.py
@@ -31,28 +31,6 @@ INSTALLED_APPS = [
     "finance",
 ]
 
-INSTALLED_APPS = [
-    app for app in INSTALLED_APPS
-    if app not in (
-        "django.contrib.admin",
-        "django.contrib.admindocs",
-        # plus any other admin-only apps you added
-        # Django
-        "django.contrib.auth",
-        "django.contrib.contenttypes",
-        "django.contrib.sessions",
-        "django.contrib.messages",
-        "django.contrib.staticfiles",
-        # 3rd-party
-        "rest_framework",
-        "rest_framework_simplejwt",
-        "django_filters",
-        # Local apps
-        "accounts",
-        "finance",
-    )
-]
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
