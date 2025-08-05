f55f0c30cf5ffec10b8b855f2ce589b4f8389cb5
SandyAbdullahi <ab2d222@gmail.com>
2025-08-05 13:37:46 +0300

    make migrations

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 export DJANGO_SETTINGS_MODULE=core.settings_ci   # 👈
 
+rm finance/migrations/0*.py && rm accounts/migrations/0*.py
 python manage.py makemigrations accounts finance
 python manage.py migrate --no-input
 gunicorn core.wsgi:application --log-file -
